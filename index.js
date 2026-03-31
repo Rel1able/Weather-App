d47c4ec99e4cce8ec6e8b06fce63e271254b420b
Yaroslav <yaroslav.shebovovkk@gmail.com>
2026-03-31 14:24:16 +0200

    refactor: migrate code from JavaScript to TypeScript

--- index.js
+++ index.js
@@ -1,72 +1,64 @@
-const address = document.getElementById("address");
-const temperature = document.getElementById("temperature");
-const humidity = document.getElementById("humidity");
-const wind = document.getElementById("wind");
-const precipitations = document.getElementById("precipitations");
+"use strict";
+const addressDiv = document.getElementById("address");
+const temperatureDiv = document.getElementById("temperature");
+const humidityDiv = document.getElementById("humidity");
+const windDiv = document.getElementById("wind");
+const precipitationsDiv = document.getElementById("precipitations");
 const weatherData = document.querySelector(".weather-data");
-
 const weatherIcon = document.querySelector(".weather-icon");
-
 async function fetchWeatherData() {
     try {
-        const cityName = document.getElementById("cityName").value.toLowerCase();
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=KQYWTLZ3T34A25ZMDLD69TJM6`, {mode: "cors"})
-    if (!response.ok) {
-        throw new Error("Failed to Fetch the data");
+        const cityInput = document.getElementById("cityName");
+        const cityName = cityInput.value.toLowerCase();
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=KQYWTLZ3T34A25ZMDLD69TJM6`, { mode: "cors" });
+        if (!response.ok) {
+            throw new Error("Failed to Fetch the data");
         }
-        
         const data = await response.json();
         console.log(data);
-
-
         const cityAddress = data.resolvedAddress;
-
         const cityTemperature = data.currentConditions.temp;
-        
         const cityHumidity = data.currentConditions.humidity;
-
-        const windSpeed = data.currentConditions.windspeed;
-
-        const probabilityOfPrecipitation = data.currentConditions.precipprob;
-
+        const cityWind = data.currentConditions.windspeed;
+        const precipitations = data.currentConditions.precipprob;
         const iconPath = data.currentConditions.icon;
         console.log(iconPath);
-
-        setConditions(cityAddress, cityTemperature, cityHumidity, windSpeed, probabilityOfPrecipitation, iconPath);
-
-
+        setConditions({
+            cityAddress,
+            cityTemperature,
+            cityHumidity,
+            cityWind,
+            precipitations,
+            iconPath,
+        });
     }
     catch (error) {
         alert("Failed to fetch weather data, please try again");
     }
 }
-
 const form = document.querySelector("form");
-
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const inputValue = document.getElementById("cityName").value.trim();
-    if (inputValue.length < 2 || /\d/.test(inputValue)) {
-        alert("Please enter a valid city name")
-    } else {
+    const inputValue = document.getElementById("cityName");
+    const cityName = inputValue.value.trim();
+    if (cityName.length < 2 || /\d/.test(cityName)) {
+        alert("Please enter a valid city name");
+    }
+    else {
         fetchWeatherData();
     }
-})
-
-
-function setConditions(cityAddress, cityTemperature, cityHumidity, cityWind, cityPrecipitations, weatherIconPath) {
-    address.textContent = cityAddress;
-    temperature.textContent = `Temperature: ${Math.round(convertToCelsius(cityTemperature))} °C`;
-    humidity.textContent = `Humidity: ${cityHumidity} %`;
-    wind.textContent = `Wind Speed: ${cityWind} km/h`;
-    precipitations.textContent = `Precipitations: ${cityPrecipitations} %`;
-    weatherIcon.src = `weather-icons/${weatherIconPath}.svg`;
+});
+function setConditions(conditions) {
+    const { cityAddress, cityTemperature, cityHumidity, cityWind, precipitations, iconPath, } = conditions;
+    addressDiv.textContent = cityAddress;
+    temperatureDiv.textContent = `Temperature: ${Math.round(convertToCelsius(cityTemperature))} °C`;
+    humidityDiv.textContent = `Humidity: ${cityHumidity} %`;
+    windDiv.textContent = `Wind Speed: ${cityWind} km/h`;
+    precipitationsDiv.textContent = `Precipitations: ${precipitations} %`;
+    weatherIcon.src = `weather-icons/${iconPath}.svg`;
     weatherData.style.display = "flex";
 }
-
-
 function convertToCelsius(temp) {
-    let celsius = (parseInt(temp) - 32) / 1.8;
+    let celsius = (temp - 32) / 1.8;
     return celsius;
 }
-
---
+++ index.ts
@@ -0,0 +1,96 @@
+const addressDiv = document.getElementById("address") as HTMLDivElement;
+const temperatureDiv = document.getElementById("temperature") as HTMLDivElement;
+const humidityDiv = document.getElementById("humidity") as HTMLDivElement;
+const windDiv = document.getElementById("wind") as HTMLDivElement;
+const precipitationsDiv = document.getElementById(
+  "precipitations",
+) as HTMLDivElement;
+const weatherData = document.querySelector(".weather-data") as HTMLDivElement;
+
+const weatherIcon = document.querySelector(".weather-icon") as HTMLImageElement;
+
+async function fetchWeatherData() {
+  try {
+    const cityInput = document.getElementById("cityName") as HTMLInputElement;
+    const cityName = cityInput.value.toLowerCase();
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=KQYWTLZ3T34A25ZMDLD69TJM6`,
+      { mode: "cors" },
+    );
+    if (!response.ok) {
+      throw new Error("Failed to Fetch the data");
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    const cityAddress = data.resolvedAddress;
+
+    const cityTemperature = data.currentConditions.temp;
+
+    const cityHumidity = data.currentConditions.humidity;
+
+    const cityWind = data.currentConditions.windspeed;
+
+    const precipitations = data.currentConditions.precipprob;
+
+    const iconPath = data.currentConditions.icon;
+    console.log(iconPath);
+
+    setConditions({
+      cityAddress,
+      cityTemperature,
+      cityHumidity,
+      cityWind,
+      precipitations,
+      iconPath,
+    });
+  } catch (error) {
+    alert("Failed to fetch weather data, please try again");
+  }
+}
+
+const form = document.querySelector("form") as HTMLFormElement;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = document.getElementById("cityName") as HTMLInputElement;
+  const cityName = inputValue.value.trim();
+  if (cityName.length < 2 || /\d/.test(cityName)) {
+    alert("Please enter a valid city name");
+  } else {
+    fetchWeatherData();
+  }
+});
+
+type Conditions = {
+  cityAddress: string;
+  cityTemperature: number;
+  cityHumidity: number;
+  cityWind: number;
+  precipitations: number;
+  iconPath: string;
+};
+
+function setConditions(conditions: Conditions) {
+  const {
+    cityAddress,
+    cityTemperature,
+    cityHumidity,
+    cityWind,
+    precipitations,
+    iconPath,
+  } = conditions;
+  addressDiv.textContent = cityAddress;
+  temperatureDiv.textContent = `Temperature: ${Math.round(convertToCelsius(cityTemperature))} °C`;
+  humidityDiv.textContent = `Humidity: ${cityHumidity} %`;
+  windDiv.textContent = `Wind Speed: ${cityWind} km/h`;
+  precipitationsDiv.textContent = `Precipitations: ${precipitations} %`;
+  weatherIcon.src = `weather-icons/${iconPath}.svg`;
+  weatherData.style.display = "flex";
+}
+
+function convertToCelsius(temp: number) {
+  let celsius = (temp - 32) / 1.8;
+  return celsius;
+}
